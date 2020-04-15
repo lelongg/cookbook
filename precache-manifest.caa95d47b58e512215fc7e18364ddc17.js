72c1d15b7556235dbcd0200d7b039d47eff8c8c5
Gérald Lelong <gerald.lelong@easymov.fr>
2020-04-15 20:10:44 +0200

    Updates

---
+++ precache-manifest.caa95d47b58e512215fc7e18364ddc17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7aa03ffe943522afe9aecfd985ab25d",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "be55d85a096df0bb30ad",
+    "url": "/cookbook/static/css/main.2fde1d6e.chunk.css"
+  },
+  {
+    "revision": "d43e1a92179bca777bcd",
+    "url": "/cookbook/static/js/2.8b18c6e9.chunk.js"
+  },
+  {
+    "revision": "cf0a4dc1778678aa1f9fc33e34dd2d4d",
+    "url": "/cookbook/static/js/2.8b18c6e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be55d85a096df0bb30ad",
+    "url": "/cookbook/static/js/main.3c5c644b.chunk.js"
+  },
+  {
+    "revision": "40a109fe504fe85c714e",
+    "url": "/cookbook/static/js/runtime-main.a1ff040f.js"
+  },
+  {
+    "revision": "b7d5240da8da217f44fda50bc432bb82",
+    "url": "/cookbook/static/media/hero.b7d5240d.jpg"
+  }
+]);
